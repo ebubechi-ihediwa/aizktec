633dcacb4b7c67efd13a59346a602664b57debda
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-03-14 22:43:36 -0600

    fix: fit build errors

--- packages/nextjs/aizktec/src/components/cards/FeatureCard.tsx
+++ packages/nextjs/aizktec/src/components/cards/FeatureCard.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 interface FeatureCardProps {
